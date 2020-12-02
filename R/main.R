2fa845ca9950d086d4ad0f2e73fb76c2487541a4
ewalsh3 <ewalsh3@ncsu.edu>
2020-12-02 13:47:12 -0500

    Update main.R

--- R/main.R
+++ R/main.R
@@ -1,10 +1,25 @@
-
-	feature.selection <- function(mtx, n_genes, lower_bound, upper_bound){
-		require(tidyverse)
-		require(philentropy)
-		# mtx <- as.data.frame(mtx)
-		mtx[mtx > 0] <- 1
-		mtx <- mtx[rowSums(mtx) > 1,]
-		seeds <- seed.find(mtx, n_genes, lower_bound, upper_bound)
-		return(bind_rows(lapply(seeds, FUN=correlation.calc, mtx=mtx)))
-	}
+#' Co-occurrence feature selection
+#'
+#' Search for seed genes in the gene expression data
+#' Selecting features that form significant correlations with seed genes 
+#'
+# @param mtx raw UMI count gene expression matrix
+# @param n_seeds number of seeds genes to use in feature selection (estimate would be # cell types)
+# @param lower_bound b/w 0 and 1; search for seeds in genes detected in > lower_bound fraction of cells 
+# @param uppe_bound b/w 0 and 1; search for seeds in genes detected in < upper_bound fraction of cells 
+# @return dataframe consisting of significant correlations between genes
+feature.selection <- function(
+mtx, 
+n_seeds=5, 
+lower_bound=0.15, 
+upper_bound=0.85
+){
+require(tidyverse)
+require(philentropy)
+## convert count data to binary 
+mtx[mtx > 0] <- 1
+## filter any undetected genes
+mtx <- mtx[rowSums(mtx) > 1,]
+seeds <- seed.find(mtx, n_seeds, lower_bound, upper_bound)
+return(bind_rows(lapply(seeds, FUN=correlation.calc, mtx=mtx)))
+}
